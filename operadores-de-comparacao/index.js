15e719282a7d6d69aad003504693d0d0bdfaf303
Maycon Gabriel | Front End Developer <115764624+maycony@users.noreply.github.com>
2024-11-26 20:00:43 -0300

    Add files via upload

---
+++ operadores-de-comparacao/index.js
@@ -0,0 +1,11 @@
+const x = 5
+const y = 5
+
+if (x > y) {
+    console.log(`${x} é maior que ${y}`)
+} else if (x < y) {
+    console.log(`${x} é menor que ${y}`)
+
+} else {
+    console.log(`${x} é igual ${y}`)
+}
---
+++ operadores-de-comparacao/readme.md
@@ -0,0 +1,4 @@
+## Operadores de Comparação
+
+Crie duas variáveis x e y. Verifique se x é maior que y, se são iguais e se x é 
+menor ou igual a y. Imprima os resultados de cada comparação.
